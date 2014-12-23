7398059aabe6c6058c022d3a4c8754f9403aef0b
Monji Dolon <mdolon@gmail.com>
2014-12-23 16:24:58 -0500

    Using local API for testing

--- lib/gemtastic.rb
+++ lib/gemtastic.rb
@@ -5,6 +5,8 @@ require 'fileutils'
 require 'gemnasium/parser'
 require 'pry'
 require 'tempfile'
+require 'open-uri'
+require 'json'
 
 class Gemtastic
   def initialize
@@ -26,9 +28,11 @@ class Gemtastic
     gemfile.dependencies.each_with_index.map {|x, i| gemlist << [x.name, x.requirement.as_list, x.groups]}
 
     # Sort by groups and group by uniques
-    gemlist = gemlist.sort_by {|gem| gem[2].to_s }.group_by {|gem| gem[2].uniq}
+    # gemlist = gemlist.sort_by {|gem| gem[2].to_s }.group_by {|gem| gem[2].uniq}
 
-    # TODO: Fetch gem info from API server
+    # Fetch gem info from API server
+    query = gemlist.map{|x| x[0]}.join(',')
+    @gemdict = JSON.load(open("http://0.0.0.0:3000/gems/#{query}"))
 
     # iterate through server and fill in gem info
     temp_file = Tempfile.new(File.dirname(__FILE__) + "Gemfile.test.tmp")
@@ -47,63 +51,74 @@ class Gemtastic
     comment = "\n"
 
     # Add prerequisite tabs
-    line[/\A */].size.times { comment += " " }
+    tab_spaces = ""
+    line[/\A */].size.times { tab_spaces += " " }
 
-    # Add some arbitrary comment
-    comment += "# This is the best comment evar."
-    comment += "\n"
-    return comment+line
+    gemline = Gemnasium::Parser::Gemfile.new(line)
+
+    if gemline.dependencies.length > 0
+      geminfo = @gemdict.find {|gem| gem["name"] == gemline.dependencies.first.name }
+
+      if geminfo
+        comment += "#{tab_spaces}# #{geminfo['info']}\n" if geminfo.has_key?("info")
+        return comment+line
+      else
+        return line
+      end
+    else
+      return line
+    end
   end
 
-  def process_old(gemfile)
-    # Assign gemfile path relative to location of execution
-    local_gemfile = File.dirname(__FILE__) + "/../Gemfile.test"
+  # def process_old(gemfile)
+  #   # Assign gemfile path relative to location of execution
+  #   local_gemfile = File.dirname(__FILE__) + "/../Gemfile.test"
 
-    # Exit if file doesn't exist
-    abort("Could not find the file, exiting") unless File.file?(local_gemfile)
+  #   # Exit if file doesn't exist
+  #   abort("Could not find the file, exiting") unless File.file?(local_gemfile)
 
-    # Create new empty array, we'll store the list of gems here
-    gemlist ||= []
+  #   # Create new empty array, we'll store the list of gems here
+  #   gemlist ||= []
 
-    # Load the gems in the gemfile, stash each gem and it's version
-    gemfile = Gemnasium::Parser::Gemfile.new(File.open(local_gemfile).read)
-    gemfile.dependencies.each_with_index.map {|x, i| gemlist << [x.name, x.requirement.as_list, x.groups]}
+  #   # Load the gems in the gemfile, stash each gem and it's version
+  #   gemfile = Gemnasium::Parser::Gemfile.new(File.open(local_gemfile).read)
+  #   gemfile.dependencies.each_with_index.map {|x, i| gemlist << [x.name, x.requirement.as_list, x.groups]}
 
-    # Sort by groups and group by uniques
-    gemlist = gemlist.sort_by {|gem| gem[2].to_s }.group_by {|gem| gem[2].uniq}
+  #   # Sort by groups and group by uniques
+  #   gemlist = gemlist.sort_by {|gem| gem[2].to_s }.group_by {|gem| gem[2].uniq}
 
-    # TODO: Fetch gem info from API server
+  #   # TODO: Fetch gem info from API server
 
-    # Find the source(s)
-    source = ""
-    File.open(local_gemfile, 'r') do |f|
-      f.each_line do |line|
-        if line.include?("source")
-          source += line
-        end
-      end
-    end
+  #   # Find the source(s)
+  #   source = ""
+  #   File.open(local_gemfile, 'r') do |f|
+  #     f.each_line do |line|
+  #       if line.include?("source")
+  #         source += line
+  #       end
+  #     end
+  #   end
 
-    temp_file = Tempfile.new(File.dirname(__FILE__) + "Gemfile.test.tmp")
-    temp_file.puts source
+  #   temp_file = Tempfile.new(File.dirname(__FILE__) + "Gemfile.test.tmp")
+  #   temp_file.puts source
 
-    gemlist.each do |group, value|
-      unless group.include?(:default)
-        group_names = group.map{|x| ":"+x.to_s}.join(',')
-        puts "group #{group_names} do\n"
-      end
-    end
+  #   gemlist.each do |group, value|
+  #     unless group.include?(:default)
+  #       group_names = group.map{|x| ":"+x.to_s}.join(',')
+  #       puts "group #{group_names} do\n"
+  #     end
+  #   end
 
-    # temp_file.close
-    # FileUtils.mv(temp_file.path, local_gemfile)
+  #   # temp_file.close
+  #   # FileUtils.mv(temp_file.path, local_gemfile)
     
-    # File.open(local_gemfile, "w+") do |file|
-    #   file.each_line do |line|
-    #     file.puts "#comment here\n" + line
-    #   end
-    # end
+  #   # File.open(local_gemfile, "w+") do |file|
+  #   #   file.each_line do |line|
+  #   #     file.puts "#comment here\n" + line
+  #   #   end
+  #   # end
 
-  end
+  # end
 end
 
 Gemtastic.new
