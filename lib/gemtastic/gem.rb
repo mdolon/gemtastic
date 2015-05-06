89cd38d79eda91721373f84396e284a66280edb6
Chris DiMartino <vwchris@gmail.com>
2015-05-06 11:45:56 -0400

    Rewrite branch - initial commit
      - Rewrite code more modularly
      - Added lots o tests
      - Added lots of parsing
      - Added lots of objects

---
+++ lib/gemtastic/gem.rb
@@ -0,0 +1,34 @@
+module Gemtastic
+  class Gem
+    attr_reader :gem, :params
+
+    def initialize gem, params=nil
+      @gem    = gem
+      @params = params.empty? ? nil : params
+    end
+
+    def annotate
+      "# Gemtastic isnt it??! #"
+    end
+
+    def to_s
+      [
+        annotate,
+        ["gem '#{gem}'", params].compact.join(",")
+      ].join("\n")
+    end
+
+    def self.from_s string
+      unless gem_string? string
+        raise Exception.new("Bad string passed - not gem format")
+      end
+      gem, match, params = string.partition(/(\Z|,)/)
+      gem = /"([^"]+)"/.match(gem.gsub("'", '"'))[1]
+      self.new gem, params
+    end
+
+    def self.gem_string? string
+      !!(string =~ /\Agem/)
+    end
+  end
+end
--- spec/gemtastic_spec.rb
+++ spec/gemtastic_spec.rb
@@ -1,7 +1,141 @@
 require 'spec_helper'
+def gemfile_string
+  <<-EOL
+source 'https://rubygems.org'
 
-describe Gemtastic do
-  subject { Gemtastic.new }
+## This is a comment and should stay in the file
+# This is another comment
+
+# The bundler gem - my own comment
+gem 'bundler'
+
+gem 'rspec' # Rspec gem
+
+gem 'rails', require: 'rails', branch: 'develop' # With options
+  EOL
+end
+
+module Gemtastic
+  describe VERSION do
+    it "Should be a string with format x.x.x" do
+      expect(VERSION).to match /\d+\.\d+\.\d+/
+    end
+  end
+
+  describe Gemfile do
+    let(:gemfile) { Gemfile.new gemfile_string }
+
+    describe "#initialize" do
+      it "should take a Gemfile content string to initialize" do
+        expect(Gemfile.new(gemfile_string)).to be_an_instance_of Gemfile
+      end
+    end
+
+    describe "#parse_gemfile" do
+      let(:parse_gemfile) {
+        gemfile.parse_gemfile
+      }
+
+      it "Should return an array" do
+        expect(parse_gemfile).to be_an Array
+      end
+
+      it "Should have a Source object as the first element" do
+        expect(parse_gemfile.first).to be_a Source
+      end
+
+      it "Should have a string object as the second object" do
+        expect(parse_gemfile[1]).to eq ""
+      end
+
+      it "Should have a Gem object as the 7th object" do
+        expect(parse_gemfile[6]).to be_a Gem
+      end
+    end
+
+    describe "#to_s" do
+      it "Should be correct!" do
+        expect(gemfile.to_s).to eq <<-EOL.chomp
+source 'https://rubygems.org'
+
+## This is a comment and should stay in the file
+# This is another comment
+
+# The bundler gem - my own comment
+# Gemtastic isnt it??! #
+gem 'bundler'
+
+# Gemtastic isnt it??! #
+gem 'rspec'
+
+# Gemtastic isnt it??! #
+gem 'rails', require: 'rails', branch: 'develop' # With options
+        EOL
+      end
+    end
+  end
+
+  describe Gem do
+    let(:good_string) { "gem 'gemmy', extra: blat" }
+    let(:bad_string)  { "xgem 'gemmy', extra: blat" }
+
+    describe ".gem_string?" do
+      it "should return true for: gem 'something'" do
+        expect(Gem.gem_string?('gem: "something"')).to be true
+      end
+
+      it 'should return false for: xgem "something"' do
+        expect(Gem.gem_string?('xgem: "something"')).to be false
+      end
+    end
+
+    describe ".from_s" do
+      it "should return a new Gem when given a good string" do
+        expect(Gem.from_s good_string).to be_a Gem
+      end
+
+      it "should raise an exception when given a bad string" do
+        expect{ Gem.from_s bad_string }.to raise_error
+      end
+    end
+
+    describe "#to_s" do
+      it "should be correct" do
+        expect(Gem.from_s(good_string).to_s).to eq <<-EOL.chomp
+# Gemtastic isnt it??! #
+gem 'gemmy', extra: blat
+        EOL
+      end
+    end
+  end
+
+  describe Source do
+    describe ".source_string?" do
+      it "should return true for: source 'something'" do
+        expect(Source.source_string?('source: "something"')).to be true
+      end
+
+      it 'should return false for: xsource "something"' do
+        expect(Source.source_string?('xsource: "something"')).to be false
+      end
+    end
+
+    describe ".from_s" do
+      let(:good_string) { "source 'gemmy'" }
+      let(:bad_string)  { "xsource 'gemmy'" }
+
+      it "should return a new Source when given a good string" do
+        expect(Source.from_s good_string).to be_a Source
+      end
+
+      it "should raise an exception when given a bad string" do
+        expect{ Source.from_s bad_string }.to raise_error
+      end
+    end
+  end
+end
+  # let(:gemfile) { gemfile_string }
+  # subject { Gemtastic.new(gemfile) }
 
   # describe '#process' do
   #   let(:input) { "source 'https://rubygems.org'\ngemspec" }
@@ -21,4 +155,4 @@ describe Gemtastic do
   #   #   expect(output).to end_with 'wow.'
   #   # end
   # end
-end
+# end
